8e9fe4bc4251d843e5e2f0cd4f44909239494890
sangjun <tedtid@naver.com>
2025-07-23 20:22:05 +0900

    feat: EmailVerificationService의 실제 이메일로 인증 코드 보내는 기능 추가

--- src/main/java/com/example/planmate/dto/SendEmailResponse.java
+++ src/main/java/com/example/planmate/dto/SendEmailResponse.java
@@ -1,4 +1,10 @@
 package com.example.planmate.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SendEmailResponse extends CommonResponse{
+    boolean isVerificationSent;
 }
--- src/main/java/com/example/planmate/service/EmailVerificationService.java
+++ src/main/java/com/example/planmate/service/EmailVerificationService.java
@@ -8,23 +8,48 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
+import java.security.SecureRandom;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class EmailVerificationService {
     private final UserRepository userRepository;
+    private final JavaMailSender mailSender;
+    private final SecureRandom secureRandom;
+
     private final Cache<String, EmailVerification> verificationCache = CacheBuilder.newBuilder()
             .expireAfterWrite(5, TimeUnit.MINUTES)
             .build();
 
     public SendEmailResponse sendVerificationCode(String email) {
         SendEmailResponse response = new SendEmailResponse();
-        int code = 12345; // 예: "123456"
+
+        //이메일 정규식 검증
+
+        if(userRepository.findByEmailIgnoreCase(email).isPresent()) {
+            response.setMessage("Email already in use");
+            response.setVerificationSent(false);
+            return response;
+        }
+
+        int code = secureRandom.nextInt(900000) + 100000;
         EmailVerification verification = new EmailVerification(email, code);
         verificationCache.put(email, verification);
-        // 여기서 이메일 전송 로직 추가
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("PlanMate 인증 코드입니다.");
+        message.setText("인증 코드: " + code);
+
+        mailSender.send(message);
+
+        response.setMessage("Verification code sent");
+        response.setVerificationSent(true);
+
         return response;
     }
 
@@ -49,6 +74,8 @@ public class EmailVerificationService {
             response.setEmailVerified(false);
             return response;
         }
+        verificationCache.invalidate(email);
+        response.setMessage("Verification completed successfully");
         response.setEmailVerified(true);
         return response;
     }
