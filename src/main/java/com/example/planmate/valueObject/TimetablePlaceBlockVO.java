5b4bb9b7bc431292e97a01090746ef772b5fd7ba
sangjun <tedtid@naver.com>
2025-07-24 09:01:54 +0900

    fix: TimetablePlaceBlockVO에 placeCategoryId, xLocation, yLocation이 항상 0으로 들어오는 것 수정

--- src/main/java/com/example/planmate/valueObject/TimetablePlaceBlockVO.java
+++ src/main/java/com/example/planmate/valueObject/TimetablePlaceBlockVO.java
@@ -1,5 +1,6 @@
 package com.example.planmate.valueObject;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import java.time.LocalTime;
 @Setter
 public class TimetablePlaceBlockVO {
     private int timetablePlaceBlockId;
+    @JsonProperty("placeCategoryId")
     private int placeCategoryId;
     private String placeName;
     private String placeTheme;
@@ -18,6 +20,8 @@ public class TimetablePlaceBlockVO {
     private String date;
     private LocalTime startTime;
     private LocalTime endTime;
+    @JsonProperty("xLocation")
     private double xLocation;
+    @JsonProperty("yLocation")
     private double yLocation;
 }
