6f0f432e26c9f84dda39de17306d8887b3c3dac2
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2025-08-06 17:54:40 +0900

    refactor: 장소 검색 수정

--- src/main/java/com/example/planmate/service/GetPlaceService.java
+++ src/main/java/com/example/planmate/service/GetPlaceService.java
@@ -2,6 +2,7 @@ package com.example.planmate.service;
 
 import com.example.planmate.auth.PlanAccessValidator;
 import com.example.planmate.dto.PlaceResponse;
+import com.example.planmate.entity.Plan;
 import com.example.planmate.externalAPI.GoogleMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,21 +18,28 @@ public class GetPlaceService {
 
     public PlaceResponse getTourPlace(int userId, int planId) throws IOException {
         PlaceResponse response = new PlaceResponse();
-        String travelName = planAccessValidator.validateUserHasAccessToPlan(userId, planId).getTravel().getTravelName();
-        response.addPlace(googleMap.getTourPlace(travelName + " " + "관광지"));
+        Plan plan = planAccessValidator.validateUserHasAccessToPlan(userId, planId);
+        String travelCategoryName = plan.getTravel().getTravelCategory().getTravelCategoryName();
+        String travelName = plan.getTravel().getTravelName();
+        response.addPlace(googleMap.getTourPlace(travelCategoryName + " " +travelName + " " + "관광지"));
         return response;
     }
     public PlaceResponse getLodgingPlace(int userId, int planId) throws IOException {
         PlaceResponse response = new PlaceResponse();
-        String travelName = planAccessValidator.validateUserHasAccessToPlan(userId, planId).getTravel().getTravelName();
-        response.addPlace(googleMap.getLodgingPlace(travelName + " " + "숙소"));
+        Plan plan = planAccessValidator.validateUserHasAccessToPlan(userId, planId);
+        String travelCategoryName = plan.getTravel().getTravelCategory().getTravelCategoryName();
+        String travelName = plan.getTravel().getTravelName();
+        response.addPlace(googleMap.getLodgingPlace(travelCategoryName + " " +travelName + " " + "숙소"));
         return response;
     }
     public PlaceResponse getRestaurantPlace(int userId, int planId) throws IOException {
         PlaceResponse response = new PlaceResponse();
-        String travelName = planAccessValidator.validateUserHasAccessToPlan(userId, planId).getTravel().getTravelName();
-        response.addPlace(googleMap.getRestaurantPlace(travelName + " " + "식당"));
+        Plan plan = planAccessValidator.validateUserHasAccessToPlan(userId, planId);
+        String travelCategoryName = plan.getTravel().getTravelCategory().getTravelCategoryName();
+        String travelName = plan.getTravel().getTravelName();
+        response.addPlace(googleMap.getRestaurantPlace(travelCategoryName + " " +travelName + " " + "식당"));
         return response;
     }
+    
 
 }
