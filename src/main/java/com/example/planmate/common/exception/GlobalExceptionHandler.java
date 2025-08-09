2345b21e1786d95566906334db112b9731ef2295
sangjun <tedtid@naver.com>
2025-08-09 16:22:50 +0900

    feat: 설정한 에러 메세지가 있을시 기본 에러 메세지가 아닌 설정한 에러 메세지를 반환하도록 추가

--- src/main/java/com/example/planmate/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/planmate/common/exception/GlobalExceptionHandler.java
@@ -9,33 +9,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private String getErrorMessage(Exception ex, String defaultMessage) {
+        return (ex.getMessage() != null && !ex.getMessage().isBlank())
+                ? ex.getMessage()
+                : defaultMessage;
+    }
+
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handleUnauthorized(UnauthorizedException ex) {
-        System.out.println(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰이 인증되지 않았습니다");
+        String message = getErrorMessage(ex, "토큰이 인증되지 않았습니다");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUnauthorized(UserNotFoundException ex) {
-        System.out.println(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용자를 찾을 수 없습니다");
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        String message = getErrorMessage(ex, "사용자를 찾을 수 없습니다");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
-        System.out.println(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 값이 들어왔습니다");
+        String message = getErrorMessage(ex, "잘못된 값이 들어왔습니다");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
-        System.out.println(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("접근 권한이 없습니다");
+        String message = getErrorMessage(ex, "접근 권한이 없습니다");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAll(Exception ex) {
-        System.out.println(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+        String message = getErrorMessage(ex, "서버 오류가 발생했습니다.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
     }
 }
