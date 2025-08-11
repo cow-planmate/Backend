4bf581908f919bcf7e93dc0ee1bb34152d7f0d58
sangjun <tedtid@naver.com>
2025-08-12 01:26:03 +0900

    fix: cors 설정에서 https://frontendplanmate-8sh5ish0j-donghyeoks-projects-dd8e94be.vercel.app/ 마지막에 / 삭제

--- src/main/java/com/example/planmate/common/config/CorsGlobalConfig.java
+++ src/main/java/com/example/planmate/common/config/CorsGlobalConfig.java
@@ -23,7 +23,7 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "https://www.planmate.site",
                         "https://planmate.site",
-                        "https://frontendplanmate-8sh5ish0j-donghyeoks-projects-dd8e94be.vercel.app/")
+                        "https://frontendplanmate-8sh5ish0j-donghyeoks-projects-dd8e94be.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
