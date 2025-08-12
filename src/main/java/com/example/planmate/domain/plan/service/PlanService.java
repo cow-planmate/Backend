3009344a1819119a9f0880247c2b2e458baff9e7
sangjun <tedtid@naver.com>
2025-08-12 19:13:57 +0900

    feat: 일정 이름 변경 기능 소유주만 가능하도록 변경

--- src/main/java/com/example/planmate/domain/plan/service/PlanService.java
+++ src/main/java/com/example/planmate/domain/plan/service/PlanService.java
@@ -156,6 +156,12 @@ public class PlanService {
         Plan plan = planAccessValidator.validateUserHasAccessToPlan(userId, planId);
         boolean exists = planRepository.existsByUser_UserIdAndPlanName(userId, name);
 
+        if(userId != plan.getUser().getUserId()){
+            response.setEdited(false);
+            response.setMessage("이름 변경 권한이 없습니다.");
+            return response;
+        }
+
         if(exists && !plan.getPlanName().equals(name)) {
             response.setEdited(false);
             response.setMessage("이미 동일한 이름의 일정이 존재합니다.");
