db749df1a2ab6a6dfa28b17a3a3b7143cac0055f
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2025-12-01 03:51:34 +0900

    임시저장

--- app/src/main/java/com/example/planmate/PlanMateApplication.java
+++ app/src/main/java/com/example/planmate/PlanMateApplication.java
@@ -2,8 +2,11 @@ package com.example.planmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.planmate", "com.sharedsync"})
+
 public class PlanMateApplication {
 
     public static void main(String[] args) {
