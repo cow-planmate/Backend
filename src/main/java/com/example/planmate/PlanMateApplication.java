55ba006b8a136e2a30adfa69da00b50f3ac7e226
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2026-01-11 18:36:29 +0900

    refactor: 한국 시간으로 변경

--- src/main/java/com/example/planmate/PlanMateApplication.java
+++ src/main/java/com/example/planmate/PlanMateApplication.java
@@ -3,8 +3,16 @@ package com.example.planmate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PlanMateApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PlanMateApplication.class, args);
