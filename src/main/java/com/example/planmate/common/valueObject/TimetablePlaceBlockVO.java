cb39f6d0a38eea2fb0cfc17820abe6133b2ed705
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2026-01-20 17:03:25 +0900

    fix: TimetablePlaceBlockVO 생성자

--- src/main/java/com/example/planmate/common/valueObject/TimetablePlaceBlockVO.java
+++ src/main/java/com/example/planmate/common/valueObject/TimetablePlaceBlockVO.java
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TimetablePlaceBlockVO {
     private int timeTableId;
     private Integer timetablePlaceBlockId;
