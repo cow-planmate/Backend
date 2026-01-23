4c1a76bdcd6d31b4cf42f4f77dde7cd8d9230b24
sangjun <tedtid@naver.com>
2026-01-23 19:33:44 +0900

    fix: V5 if문 추가

--- src/main/resources/db/migration/V5__create_place_search_condition.sql
+++ src/main/resources/db/migration/V5__create_place_search_condition.sql
@@ -1,21 +1,21 @@
-CREATE TABLE place_search_condition (
-                                        condition_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS place_search_condition (
+                                                      condition_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
     -- 추천 조건 (논리적 참조)
-                                        travel_id INT NOT NULL,
-                                        place_category_id INT NOT NULL,
-                                        preferred_theme_id INT,
+                                                      travel_id INT NOT NULL,
+                                                      place_category_id INT NOT NULL,
+                                                      preferred_theme_id INT,
 
     -- 검색 조건 조합 키 (서버에서 생성)
-                                        cache_key VARCHAR(128) NOT NULL,
+                                                      cache_key VARCHAR(128) NOT NULL,
 
     -- 캐시 관리
-                                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                        expired_at TIMESTAMP NOT NULL
-);
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expired_at TIMESTAMP NOT NULL
+    );
 
-CREATE UNIQUE INDEX ux_place_search_condition_cache_key
+CREATE UNIQUE INDEX IF NOT EXISTS ux_place_search_condition_cache_key
     ON place_search_condition (cache_key);
 
-CREATE INDEX idx_place_search_condition_expired
+CREATE INDEX IF NOT EXISTS idx_place_search_condition_expired
     ON place_search_condition (expired_at);
